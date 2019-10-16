c0833b8877a3179720e302f568bcd996b603ac1a
Thibault Jamet <tjamet@users.noreply.github.com>
2019-10-16 11:52:16 +0200

    Add tests

---
+++ main_test.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"net/http/httptest"
+	"strings"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestHandle(t *testing.T) {
+	const expectedBody = `{
+		"URL": {
+		  "Scheme": "http",
+		  "Opaque": "",
+		  "User": null,
+		  "Host": "localhost:8080",
+		  "Path": "/some/path",
+		  "RawPath": "",
+		  "ForceQuery": false,
+		  "RawQuery": "",
+		  "Fragment": ""
+		},
+		"Method": "GET",
+		"Proto": "HTTP/1.1",
+		"Header": {
+		  "Someheader": [
+			"value"
+		  ]
+		},
+		"Host": "localhost:8080"
+	  }`
+	r := httptest.NewRequest("GET", "http://localhost:8080/some/path", strings.NewReader("body"))
+	r.Header.Set("SomeHeader", "value")
+	w := httptest.NewRecorder()
+	handler(w, r)
+	assert.JSONEq(t, expectedBody, w.Body.String())
+}
